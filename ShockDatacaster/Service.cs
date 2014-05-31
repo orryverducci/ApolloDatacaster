d923d28c8167afb66b82f319adf51ef28d610102
Orry Verducci <orry@orryverducci.co.uk>
2014-05-31 22:12:30 +0100

    Creation of setup, web server and console app

--- ShockDatacaster/Service.cs
+++ ShockDatacaster/Service.cs
@@ -12,15 +12,57 @@ namespace ShockDatacaster
 {
     public partial class Service : ServiceBase
     {
+        /// <summary>
+        /// The system service for ShockDatacaster
+        /// </summary>
         public Service()
         {
             InitializeComponent();
         }
 
+        /// <summary>
+        /// The main entry point for the application
+        /// </summary>
+        static void Main(string[] args)
+        {
+            // Create instance of the service
+            Service service = new Service();
+            
+            // Launch service
+            if (!Environment.UserInteractive) // If running as a system service
+            {
+                // Run the service
+                ServiceBase[] ServicesToRun;
+                ServicesToRun = new ServiceBase[] { service };
+                ServiceBase.Run(ServicesToRun);
+            }
+            else // Else if running as a console application
+            {
+                // Write header
+                Console.WriteLine("ShockDatacaster");
+                Console.WriteLine("---------------");
+                Console.WriteLine("Press any key to exit");
+                Console.WriteLine();
+                // Start the service
+                service.OnStart(args);
+                // Wait for key entry to exit
+                Console.ReadKey();
+                // Stop the service
+                service.OnStop();
+            }
+        }
+
+        /// <summary>
+        /// Carry out service launch
+        /// </summary>
+        /// <param name="args">Applicaton arguments</param>
         protected override void OnStart(string[] args)
         {
         }
 
+        /// <summary>
+        /// Stop the service
+        /// </summary>
         protected override void OnStop()
         {
         }
