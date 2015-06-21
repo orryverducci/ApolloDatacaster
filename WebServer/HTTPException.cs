dadc2254c1f2e29dd8294309cea87a6a62197125
Orry Verducci <orry@orryverducci.co.uk>
2015-06-21 16:52:11 +0100

    Improve HTTP Exception

--- WebServer/HTTPException.cs
+++ WebServer/HTTPException.cs
@@ -1,12 +1,13 @@
 ﻿using System;
 using System.Runtime.Serialization;
+using System.Security.Permissions;
 
 namespace WebServer
 {
     [Serializable]
     public class HTTPException : Exception
     {
-        public HTTPResponse.HTTPStatus ErrorCode { get; set; }
+        public HTTPResponse.HTTPStatus ErrorCode { get; set; } = HTTPResponse.HTTPStatus.SERVERERROR;
 
         public HTTPException() { }
 
@@ -14,20 +15,37 @@ namespace WebServer
 
         public HTTPException(string message, Exception innerException): base(message, innerException) { }
 
+        public HTTPException(HTTPResponse.HTTPStatus errorCode): base("A HTTP error has occured")
+        {
+            ErrorCode = errorCode;
+        }
+
+        public HTTPException(int errorCode) : base("A HTTP error has occured")
+        {
+            ErrorCode = (HTTPResponse.HTTPStatus)errorCode;
+        }
+
+        public HTTPException(HTTPResponse.HTTPStatus errorCode, Exception innerException) : base("A HTTP error has occured", innerException)
+        {
+            ErrorCode = errorCode;
+        }
+
+        [SecurityPermissionAttribute(SecurityAction.Demand, SerializationFormatter = true)]
         protected HTTPException(SerializationInfo info, StreamingContext context): base(info, context)
         {
             if (info != null)
             {
-                ErrorCode = (HTTPResponse.HTTPStatus)info.GetInt32("ErrorCode");
+                ErrorCode = (HTTPResponse.HTTPStatus)info.GetValue("ErrorCode", typeof(HTTPResponse.HTTPStatus));
             }
         }
 
+        [SecurityPermissionAttribute(SecurityAction.Demand, SerializationFormatter = true)]
         public override void GetObjectData(SerializationInfo info, StreamingContext context)
         {
             base.GetObjectData(info, context);
             if (info != null)
             {
-                info.AddValue("ErrorCode", (int)ErrorCode);
+                info.AddValue("ErrorCode", ErrorCode, typeof(HTTPResponse.HTTPStatus));
             }
         }
     }
