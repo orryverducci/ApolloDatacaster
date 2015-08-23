4a08997667f46b856c8ebc57b7251256de6b5c2f
Orry Verducci <orry@orryverducci.co.uk>
2015-08-23 14:32:53 +0100

    Add comments to HTTPErrorException

--- WebServer/HTTPErrorException.cs
+++ WebServer/HTTPErrorException.cs
@@ -9,22 +9,47 @@ namespace WebServer
     {
         public HTTPResponse.HTTPStatus ErrorCode { get; set; } = HTTPResponse.HTTPStatus.SERVERERROR;
 
+        /// <summary>
+        /// Initializes a new instance of the <see cref="HTTPErrorException"/> class
+        /// </summary>
         public HTTPErrorException() { }
 
+        /// <summary>
+        /// Initializes a new instance of the <see cref="HTTPErrorException"/> class with an error message to be returned
+        /// </summary>
+        /// <param name="message">The error message to be returned</param>
         public HTTPErrorException(string message): base(message) { }
 
+        /// <summary>
+        /// Initializes a new instance of the <see cref="HTTPErrorException"/> class with an error message to be returned, and an inner exception
+        /// </summary>
+        /// <param name="message">The error message to be returned</param>
+        /// <param name="innerException">The inner exception to be returned</param>
         public HTTPErrorException(string message, Exception innerException): base(message, innerException) { }
 
+        /// <summary>
+        /// Initializes a new instance of the <see cref="HTTPErrorException"/> class with the HTTP status code to be returned
+        /// </summary>
+        /// <param name="errorCode">The HTTP status code to be returned</param>
         public HTTPErrorException(HTTPResponse.HTTPStatus errorCode): base("A HTTP error has occured")
         {
             ErrorCode = errorCode;
         }
 
+        /// <summary>
+        /// Initializes a new instance of the <see cref="HTTPErrorException"/> class with the HTTP status code to be returned
+        /// </summary>
+        /// <param name="errorCode">The HTTP status code to be returned</param>
         public HTTPErrorException(int errorCode) : base("A HTTP error has occured")
         {
             ErrorCode = (HTTPResponse.HTTPStatus)errorCode;
         }
 
+        /// <summary>
+        /// Initializes a new instance of the <see cref="HTTPErrorException"/> class with the HTTP status code and the inner exception to be returned
+        /// </summary>
+        /// <param name="errorCode">The HTTP status code to be returned</param>
+        /// <param name="innerException">The inner exception to be returned</param>
         public HTTPErrorException(HTTPResponse.HTTPStatus errorCode, Exception innerException) : base("A HTTP error has occured", innerException)
         {
             ErrorCode = errorCode;
